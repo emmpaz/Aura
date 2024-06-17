102da2229d1ff9cabfd84e8a1886b020713ceb38
emmpaz <mannypaz15@gmail.com>
2024-06-17 10:27:19 -0500

    fix callback

--- src/app/lib/youtube.ts
+++ src/app/lib/youtube.ts
@@ -8,7 +8,7 @@ export const createOAuth2Client = async () => {
     const oauth2Client = new google.auth.OAuth2({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        redirectUri: process.env.AUTH_CALLBACK
+        redirectUri: process.env.GOOGLE_CALLBACK
     })
     return oauth2Client;
 }
